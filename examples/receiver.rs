d8041b6f0d794aa60e844810dbd4551532312d10
Scott Gibb <smqgibb@yahoo.com>
2024-12-19 21:26:54 +0000

    added receiver example

---
+++ examples/receiver.rs
@@ -0,0 +1,33 @@
+use paho_mqtt as mqtt;
+
+fn main() {
+    let host_address = "localhost";
+    let port = 1883;
+    let topic = "presence/room";
+
+    let create_opts = mqtt::CreateOptionsBuilder::new()
+        .server_uri(format!("tcp://{}:{}", host_address, port))
+        .client_id("receiver")
+        .finalize();
+
+    let client = mqtt::Client::new(create_opts).expect("Failed to create client");
+
+    let conn_opts = mqtt::ConnectOptionsBuilder::new()
+        .keep_alive_interval(std::time::Duration::from_secs(30))
+        .clean_session(true)
+        .finalize();
+
+    client
+        .connect(conn_opts)
+        .expect("Failed to connect, are you sure the broker is running?");
+
+    client
+        .subscribe(topic, 1)
+        .expect("Failed to subscribe, are you sure the topic is correct?");
+
+    for msg in client.start_consuming() {
+        if let Some(msg) = msg {
+            println!("Received: {:?}", msg);
+        }
+    }
+}
--- src/config.rs
+++ src/config.rs
@@ -13,14 +13,14 @@ pub struct Config {
     pub mqtt: MqttConfig,
     pub pin: u8,
 }
-
 pub fn get_config_from_env() -> Config {
-    let mqtt_host = env::var("MQTT_HOST").unwrap_or_else(|_| "localhost".to_string());
-    let mqtt_port = env::var("MQTT_PORT").unwrap_or_else(|_| "1883".to_string());
-    let mqtt_client_id = env::var("MQTT_CLIENT_ID").unwrap_or_else(|_| "ftdi".to_string());
-    let pin = env::var("PIN").unwrap_or_else(|_| "0".to_string());
-    let room = env::var("ROOM").unwrap_or_else(|_| "living-room".to_string());
-    let sensor_id = env::var("SENSOR_ID").unwrap_or_else(|_| "1".to_string());
+    let mqtt_host = env::var("MQTT_HOST").expect("MQTT_HOST environment variable not set");
+    let mqtt_port = env::var("MQTT_PORT").expect("MQTT_PORT environment variable not set");
+    let mqtt_client_id =
+        env::var("MQTT_CLIENT_ID").expect("MQTT_CLIENT_ID environment variable not set");
+    let pin = env::var("PIN").expect("PIN environment variable not set");
+    let room = env::var("ROOM").expect("ROOM environment variable not set");
+    let sensor_id = env::var("SENSOR_ID").expect("SENSOR_ID environment variable not set");
     let mqtt_topic = format!("presence/{}/{}", room, sensor_id);
     Config {
         mqtt: MqttConfig {
